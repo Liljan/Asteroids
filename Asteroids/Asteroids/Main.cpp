c032fa899a1aabff429ca0517cb6608903b37202
Liljan <marcus.t.lilja@gmail.com>
2017-10-14 00:26:17 +0200

    Locked FPS.

--- Asteroids/Asteroids/Main.cpp
+++ Asteroids/Asteroids/Main.cpp
@@ -3,10 +3,14 @@
 
 GameEngine* gameEngine = nullptr;
 
+const int TARGET_FPS = 60;
+
 int main(int argc, char* argv[])
 {
 	int currentFrameTime, lastFrameTime = 0;
 	float dt;
+
+	const int FPS_MS = 1000 / TARGET_FPS;
 
 	gameEngine = new GameEngine();
 	gameEngine->Init("Assdroids", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, false);
@@ -19,6 +23,8 @@ int main(int argc, char* argv[])
 		gameEngine->HandleEvents();
 		gameEngine->Update(dt);
 		gameEngine->Render();
+
+		SDL_Delay(FPS_MS);
 
 		lastFrameTime = SDL_GetTicks();
 	}
